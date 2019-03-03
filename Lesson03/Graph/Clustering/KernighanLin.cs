aa930df2a961f01815805ea5a188baebe11ae34c
daywee <d4v1d.nemec@gmail.com>
2019-03-03 10:59:25 +0100

    Lesson 3: Finished outer loop

--- Lesson03/Graph/Clustering/KernighanLin.cs
+++ Lesson03/Graph/Clustering/KernighanLin.cs
@@ -16,46 +16,64 @@ namespace Lesson03.Graph.Clustering
             var group2 = shuffled.Skip(shuffled.Count / 2).ToList();
 
             int initialCutSize = GetCutSize(group1, group2);
-            
+
             List<Vertex> bestNewGroup1 = null;
             List<Vertex> bestNewGroup2 = null;
             Vertex bestSwappedFromGroup1 = null;
             Vertex bestSwappedFromGroup2 = null;
             var swappedVertices = new List<Vertex>();
-            var allStates = new Dictionary<int, (List<Vertex>, List<Vertex>)>();
-            // steps
-            while (swappedVertices.Count < shuffled.Count)
+            int bestRun = initialCutSize;
+            int currentRun = bestRun;
+
+            do
             {
-                int bestCutSize = int.MaxValue;
-
-                foreach (var v1 in group1)
+                bestRun = currentRun;
+                var allStates = new Dictionary<int, (List<Vertex>, List<Vertex>)>();
+                // steps
+                while (swappedVertices.Count < shuffled.Count)
                 {
-                    if (swappedVertices.Contains(v1))
-                        continue;
-                    foreach (var v2 in group2)
-                    {
-                        if (swappedVertices.Contains(v2))
-                            continue;
+                    int bestCutSize = int.MaxValue;
 
-                        var newGroup1 = group1.Except(new[] { v1 }).Concat(new[] { v2 }).ToList();
-                        var newGroup2 = group2.Except(new[] { v2 }).Concat(new[] { v1 }).ToList();
-                        int newCutSize = GetCutSize(newGroup1, newGroup2);
-                        if (newCutSize < bestCutSize)
+                    foreach (var v1 in group1)
+                    {
+                        if (swappedVertices.Contains(v1))
+                            continue;
+                        foreach (var v2 in group2)
                         {
-                            bestCutSize = newCutSize;
-                            bestNewGroup1 = newGroup1;
-                            bestNewGroup2 = newGroup2;
-                            bestSwappedFromGroup1 = v1;
-                            bestSwappedFromGroup2 = v2;
+                            if (swappedVertices.Contains(v2))
+                                continue;
+
+                            var newGroup1 = group1.Except(new[] { v1 }).Concat(new[] { v2 }).ToList();
+                            var newGroup2 = group2.Except(new[] { v2 }).Concat(new[] { v1 }).ToList();
+                            int newCutSize = GetCutSize(newGroup1, newGroup2);
+                            if (newCutSize < bestCutSize)
+                            {
+                                bestCutSize = newCutSize;
+                                bestNewGroup1 = newGroup1;
+                                bestNewGroup2 = newGroup2;
+                                bestSwappedFromGroup1 = v1;
+                                bestSwappedFromGroup2 = v2;
+                            }
                         }
                     }
+
+                    allStates[bestCutSize] = (bestNewGroup1, bestNewGroup2);
+                    group1 = bestNewGroup1;
+                    group2 = bestNewGroup2;
+                    swappedVertices.Add(bestSwappedFromGroup1);
+                    swappedVertices.Add(bestSwappedFromGroup2);
                 }
-                allStates.Add(bestCutSize, (bestNewGroup1, bestNewGroup2));
-                group1 = bestNewGroup1;
-                group2 = bestNewGroup2;
-                swappedVertices.Add(bestSwappedFromGroup1);
-                swappedVertices.Add(bestSwappedFromGroup2);
-            }
+
+                swappedVertices = new List<Vertex>();
+
+                var bestState = allStates.OrderBy(e => e.Key).First();
+                currentRun = bestState.Key;
+                var (g1, g2) = bestState.Value;
+                group1 = g1;
+                group2 = g2;
+            } while (currentRun < bestRun);
+
+
 
         }
 
